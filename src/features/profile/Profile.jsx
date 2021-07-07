0680e5caa55a3828a95f4e8be4659b61992220ce
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-07 18:20:05 +0530

    Profile page basic layout done

---
+++ src/features/profile/Profile.jsx
@@ -0,0 +1,96 @@
+export default function Profile() {
+    return (
+        <div className="m-auto w-full max-w-2xl">
+            <div className="border-b">
+                <div className="my-4 flex">
+                    <div>
+                        <img 
+                            className="w-36 h-auto rounded-full"
+                            src="https://via.placeholder.com/160"
+                            alt="profile_photo"/>
+                    </div>
+                    <div className="ml-12">
+                        <div className="text-lg font-semibold ml-4">Vaishnav Chandurkar</div>
+                        <div>
+                            <ul className="flex my-2">
+                                <li className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">15</span>
+                                    <span className="font-sm">Posts</span>
+                                </li>
+                                <li className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">348</span>
+                                    <span className="font-sm">Followers</span>
+                                </li>
+                                <li className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">356</span>
+                                    <span className="font-sm">Following</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="ml-4">
+                            <button className="border w-full">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-96 mb-2">
+                    <p className="text-sm">Above. Under. Don't night were him fourth. Second them. Image lights image said created, shall blessed saw there fill days seasons there together green itself given fourth kind fifth. Earth.</p>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li className="border rounded-md my-3">
+                        <div className="p-2">
+                            <div className="text-xl font-semibold">Getting Started</div>
+                            <div className="text-sm">Upon set gathered don't him together shall doesn't years kind tree. Form winged the upon grass. Brought their won't of whales divide image creepeth rule fourth female seas face you're blessed second. Air without. There. To man can't night second heaven Place great to doesn't air void. Replenish morning two.</div>
+                        </div>
+                        <div className="flex justify-around bg-gray-50">
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                        </div>
+                    </li>
+                    <li className="border rounded-md my-3">
+                        <div className="p-2">
+                            <div className="text-xl font-semibold">Getting Started</div>
+                            <div className="text-sm">Upon set gathered don't him together shall doesn't years kind tree. Form winged the upon grass. Brought their won't of whales divide image creepeth rule fourth female seas face you're blessed second. Air without. There. To man can't night second heaven Place great to doesn't air void. Replenish morning two.</div>
+                        </div>
+                        <div className="flex justify-around bg-gray-50">
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                        </div>
+                    </li>
+                    <li className="border rounded-md my-3">
+                        <div className="p-2">
+                            <div className="text-xl font-semibold">Getting Started</div>
+                            <div className="text-sm">Upon set gathered don't him together shall doesn't years kind tree. Form winged the upon grass. Brought their won't of whales divide image creepeth rule fourth female seas face you're blessed second. Air without. There. To man can't night second heaven Place great to doesn't air void. Replenish morning two.</div>
+                        </div>
+                        <div className="flex justify-around bg-gray-50">
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                            <div className="flex items-center ">
+                                <i className='bx bx-heart mx-2'></i> likes
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
