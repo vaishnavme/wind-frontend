a6c93446b601182db494c675aec11b93867bb284
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-07 15:56:30 +0530

    Revert "custom color added and page update"
    This reverts commit 39f8d1f79a440583fcaa0e186153ca7580372546.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        "dark-900": "#131311",
-        "dark-800": "#2c2c2e",
-        "blue-dark": "#0a84ff",
-        "blue-light": "#32ade6"
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
