f494bc90c2f07a4dff112e66997d863454bf3040
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-09 20:33:57 +0530

    profile layout completed

--- src/features/profile/Profile.jsx
+++ src/features/profile/Profile.jsx
@@ -1,12 +1,49 @@
 import { useState, Fragment } from "react"
 
 export default function Profile() {
-    const [isProfileModel, setProfileModel] = useState(false);
 
     return (
         <Fragment>
-            <div className="m-auto w-full max-w-2xl">
-            <div className="border-b relative">
+            <div className="bg-white rounded-md shadow p-4 mt-2">
+                <div className="flex items-center justify-center md:justify-start flex-col md:flex-row">
+                    <div>
+                        <img 
+                            className="w-36 h-auto rounded-full"
+                            src="https://via.placeholder.com/160"
+                            alt="profile_photo"/>
+                    </div>
+                    <div className="mt-4 md:ml-12">
+                        <div className="text-lg font-semibold ml-4 text-center md:text-left">Vaishnav Chandurkar</div>
+                        <div>
+                            <ul className="flex my-2">
+                                <li className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">15</span>
+                                    <span className="font-sm">Posts</span>
+                                </li>
+                                <li 
+                                    className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">348</span>
+                                    <span className="font-sm">Followers</span>
+                                </li>
+                                <li 
+                                    className="flex flex-col items-center mx-4">
+                                    <span className="text-lg font-medium">356</span>
+                                    <span className="font-sm">Following</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <button className="border flex items-center justify-center w-full rounded p-1 ml-2 hover:bg-blue-600 hover:text-white duration-300 rounded hover:bg-blue-700">
+                        <i className="bx bx-cog mx-2"></i>Edit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+
+{/* <div className="bg-white shadow rounded-md p-4">
                 <div className="my-4 flex">
                     <div>
                         <img 
@@ -23,13 +60,11 @@ export default function Profile() {
                                     <span className="font-sm">Posts</span>
                                 </li>
                                 <li 
-                                    onClick={() => setProfileModel((preState) => !preState)}
                                     className="flex flex-col items-center mx-4">
                                     <span className="text-lg font-medium">348</span>
                                     <span className="font-sm">Followers</span>
                                 </li>
                                 <li 
-                                    onClick={() => setProfileModel((preState) => !preState)}
                                     className="flex flex-col items-center mx-4">
                                     <span className="text-lg font-medium">356</span>
                                     <span className="font-sm">Following</span>
@@ -46,7 +81,8 @@ export default function Profile() {
                     <p className="text-sm">Above. Under. Don't night were him fourth. Second them. Image lights image said created, shall blessed saw there fill days seasons there together green itself given fourth kind fifth. Earth.</p>
                 </div>
             </div>
-            {isProfileModel && <ProfileModel/>}
+
+
             <div>
                 <ul>
                     <li className="border rounded-md my-3">
@@ -101,45 +137,4 @@ export default function Profile() {
                         </div>
                     </li>
                 </ul>
-            </div>
-        </div>
-        </Fragment>
-    )
-}
-
-
-const ProfileModel = () => {
-    return (
-        <div>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-30 outline-none focus:outline-none">
-            <div className="relative mx-auto w-96 bg-white p-2 rounded">
-                <div className="flex justify-between">
-                    <button className="w-full bg-blue-100">Followers</button>
-                    <button className="w-full bg-gray-100 py-2 ">Following</button>
-                </div>
-                <div className="overflow-y-scroll overscroll-y-auto max-h-64 my-1">
-                    <ul className="mx-4">
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                        <li className="py-1">Josh Smith</li>
-                    </ul>
-                </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-20 bg-black"></div>
-        </div>
-    )
-}
-
+            </div> */}
