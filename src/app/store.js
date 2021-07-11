6e1f2f65c84a2969424c8fc39e0b60f4aaea79f1
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-11 18:06:52 +0530

    explore state added

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authUserReducer from "../features/auth/authSlice";
 import profileReducer from "../features/profile/profileSlice";
+import exploreReducer from "../features/expolre/exploreSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authUserReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    explore: exploreReducer
   },
 });
