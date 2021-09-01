1af49074cdc99ac163f93c732f5ef1e922146a59
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-01 16:47:06 +0530

    Feed loader animation added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,17 +1,16 @@
-import { useSelector } from "react-redux";
-import { NewPost, PostCard } from "../components";
+import { useSelector } from 'react-redux';
+import { Loader, NewPost, PostCard } from '../components';
 
 export default function Feed() {
-    const { feedPosts } = useSelector((state) => state.posts);
+    const { feedPosts, postStatus } = useSelector((state) => state.posts);
 
     return (
         <div>
-            <NewPost/>
-            {
-                feedPosts?.map((post) => (
-                    <PostCard key={post._id} post={post}/>
-                ))
-            }
+            <NewPost />
+            {postStatus === 'loading' && <Loader />}
+            {feedPosts?.map((post) => (
+                <PostCard key={post?._id} post={post} />
+            ))}
         </div>
-    )
+    );
 }
